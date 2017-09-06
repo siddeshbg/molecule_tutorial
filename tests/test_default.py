37239c42e811e958c1c3c4a77b8decf03936f0fb
siddesh.gurusiddappa <siddesh.gurusiddappa@appdynamics.com>
2017-09-06 16:07:42 +0530

    Molecule files to test a role
    This tests whether a package as been installed on the container launced
    by molcule

---
+++ tests/test_default.py
@@ -0,0 +1,16 @@
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    '.molecule/ansible_inventory').get_hosts('all')
+
+
+def test_hosts_file(File):
+    f = File('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+
+
+def test_packages(Package):
+    assert Package("aptitude").is_installed
